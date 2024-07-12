34dd91eab86f9e7a3464de37309dc409a30e031d
ERICOPAGANINI <erico.paganini@gmail.com>
2024-07-12 07:55:22 -0300

    Subida dos arquivos da API

---
+++ src/test/java/API/REST/dio/PrimeiroAPI/PrimeiroApiApplicationTests.java
@@ -0,0 +1,13 @@
+package API.REST.dio.PrimeiroAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
