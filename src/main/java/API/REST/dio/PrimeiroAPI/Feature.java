e5c26bcf94b6ad1903211e8664f4f26ba2ef77ef
ERICOPAGANINI <erico.paganini@gmail.com>
2024-07-12 14:21:33 -0300

    Criacao das classes da API

---
+++ src/main/java/API/REST/dio/PrimeiroAPI/Feature.java
@@ -0,0 +1,5 @@
+package API.REST.dio.PrimeiroAPI;
+
+public class Feature extends BaseItem {
+	
+}
---
+++ src/main/java/API/REST/dio/PrimeiroAPI/News.java
@@ -0,0 +1,8 @@
+package API.REST.dio.PrimeiroAPI;
+
+import jakarta.persistence.Entity;
+
+@Entity (name = "tb_news")
+public class News extends BaseItem{
+
+}
